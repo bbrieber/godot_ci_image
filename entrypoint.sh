54843ce1bb2d2f79479bb0d2b1e33cc8ea2921fc
Benjamin Brieber <bbrieber@users.noreply.github.com>
2024-03-14 20:48:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
+echo ls /github/workspace/project
+
+echo ls /github/workspace
+
 /godot/Godot_v4.2.1-stable_linux.x86_64 --headless --path /github/workspace/project --export-release $1 
