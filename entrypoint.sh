d4e8fd45535b4bed375807bc7ad7b67d8b0abb11
Benjamin Brieber <bbrieber@users.noreply.github.com>
2024-03-14 21:55:19 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,18 @@
 mkdir -p /github/home/.local/share/godot/export_templates/4.2.1.stable/
 cp -r /godot/templates/* /github/home/.local/share/godot/export_templates/4.2.1.stable/
 
+echo "PROJECT"
+
 echo `ls /github/workspace/project`
+
+echo "WORKSPACE"
 
 echo `ls /github/workspace`
 
+echo "Template Folder Source"
+echo `ls /godot/templates`
 
+echo "Template Folder"
 echo `ls /github/home/.local/share/godot/export_templates/4.2.1.stable/`
 
 /godot/Godot_v4.2.1-stable_linux.x86_64 --headless --path /github/workspace/project --export-release WindowsDesktop
