4ff9df1a789d3816307d3ad7a0243897747d0df2
Benjamin Brieber <bbrieber@users.noreply.github.com>
2024-04-08 00:35:20 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,23 +15,23 @@ echo $3
 
 echo "PROJECT"
 
-echo `ls /github/workspace/project`
+echo `ls  -la /github/workspace/project`
 
 echo "WORKSPACE"
 
-echo `ls /github/workspace`
+echo `ls -la /github/workspace`
 
 echo "Template Folder Source"
-echo `ls /godot/templates`
+echo `ls -la /godot/templates`
 
 
 
 
 echo "Godot Folder Source"
-echo `ls /godot`
+echo `ls -la /godot`
 
 echo "Template Folder"
-echo `ls /github/home/.local/share/godot/export_templates/4.2.1.stable/`
+echo `ls -la /github/home/.local/share/godot/export_templates/4.2.1.stable/`
 
 echo "/godot/Godot_v4.2.1-stable_linux.x86_64 --headless --path /github/workspace/$3 $2 $1"
 
