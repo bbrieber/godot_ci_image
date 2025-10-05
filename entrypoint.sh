7d69788bd2015da841be9810eb9777c8a6e1950a
Benjamin Brieber <bbrieber@users.noreply.github.com>
2025-10-05 12:30:53 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 #cp -r /godot/templates/* /github/home/.local/share/godot/export_templates/4.5.stable/
 
 #echo "/godot/Godot_v4.2.1-stable_linux.x86_64 --headless --path /github/workspace/$3 $2 $1"
-
+ls /godot
 /godot/Godot_v4.5-stable_linux.x86_64 --headless --path /github/workspace/$3 $2 $1
